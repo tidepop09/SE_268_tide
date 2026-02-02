99cbc871f20e03943ef1ccb88f674542dae321ea
tidepop09 <tidepop09@gmail.com>
2026-02-02 19:07:08 +0700

    version 1.4 pen_cal

--- tide.py
+++ tide.py
@@ -8,8 +8,9 @@ def area_cal(x, y, r, d1, d2):
     sem_cal = 0.5 * math.pi * r * r
     squ_cal = x * x
     rho_cal = 0.5 * d1 * d2
+    pen_cal = (5 * a**2) / (4 * math.tan(math.pi / 5))
     
-    return reg_cal, tri_cal, cir_cal, sem_cal, squ_cal, rho_cal
+    return reg_cal, tri_cal, cir_cal, sem_cal, squ_cal, rho_cal, pen_cal
 
 
 reg, tri, cir, sem, squ, rho = area_cal(6, 4, 3)
@@ -20,3 +21,4 @@ print(f'วงกลม = {cir}')
 print(f'ครึ่งวงกลม = {sem}')
 print(f'หาสี่เหลี่ยมจัตุรัส = {squ}')
 print(f'หาพื้นที่สี่เหลี่ยมด้านขนาน = {rho}')
+print(f'หาพื้นที่รูปห้าเหลี่ยม = {pen}')
