36b78e37ebb8925fdaa332d1efe37cb875163d6f
tidepop09 <tidepop09@gmail.com>
2026-02-02 17:24:12 +0700

    version1.2 cir_cal()

--- tide.py
+++ tide.py
@@ -1,10 +1,15 @@
 
-def area_cal(x, y):
-    reg_cal = x * y
-    tri_cal = (1/2) * x * y
-    return (reg_cal, tri_cal)
+import math
+
+def area_cal(x, y, r):
+    reg_cal = x * y                
+    tri_cal = 0.5 * x * y           
+    cir_cal = math.pi * r * r      
+    return reg_cal, tri_cal, cir_cal
 
 
-reg, tri = area_cal(6, 4)
-print(f'หาสี่เหลี่ยม {reg}')
-print(f'หาสามเหลี่ยม {tri}')
+reg, tri, cir = area_cal(6, 4, 3)
+
+print(f'หาสี่เหลี่ยม = {reg}')
+print(f'หาสามเหลี่ยม = {tri}')
+print(f'วงกลม = {cir}')
