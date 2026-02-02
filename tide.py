374463b060c3fc7184c616d91d52a012debcf4f4
tidepop09 <tidepop09@gmail.com>
2026-02-02 18:59:37 +0700

    version 1.4 rho_cal

--- tide.py
+++ tide.py
@@ -1,20 +1,22 @@
 
 import math
 
-def area_cal(x, y, r):
+def area_cal(x, y, r, d1, d2):
     reg_cal = x * y                
     tri_cal = 0.5 * x * y           
     cir_cal = math.pi * r * r
-    sem_cal = 1/2 * math.pi * r * r
+    sem_cal = 0.5 * math.pi * r * r
     squ_cal = x * x
+    rho_cal = 0.5 * d1 * d2
     
-    return reg_cal, tri_cal, cir_cal, sem_cal, squ_cal
+    return reg_cal, tri_cal, cir_cal, sem_cal, squ_cal, rho_cal
 
 
-reg, tri, cir, sem, squ = area_cal(6, 4, 3)
+reg, tri, cir, sem, squ, rho = area_cal(6, 4, 3)
 
 print(f'หาสี่เหลี่ยม = {reg}')
 print(f'หาสามเหลี่ยม = {tri}')
 print(f'วงกลม = {cir}')
 print(f'ครึ่งวงกลม = {sem}')
 print(f'หาสี่เหลี่ยมจัตุรัส = {squ}')
+print(f'หาพื้นที่สี่เหลี่ยมด้านขนาน = {rho}')
