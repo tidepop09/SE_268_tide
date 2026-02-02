51a4dc157c90b7d05cda9e33f05c1faa2d67a53e
tidepop09 <tidepop09@gmail.com>
2026-02-02 18:56:14 +0700

    version 1.3 squ_cal

--- tide.py
+++ tide.py
@@ -6,13 +6,15 @@ def area_cal(x, y, r):
     tri_cal = 0.5 * x * y           
     cir_cal = math.pi * r * r
     sem_cal = 1/2 * math.pi * r * r
+    squ_cal = x * x
     
-    return reg_cal, tri_cal, cir_cal, sem_cal
+    return reg_cal, tri_cal, cir_cal, sem_cal, squ_cal
 
 
-reg, tri, cir, sem = area_cal(6, 4, 3)
+reg, tri, cir, sem, squ = area_cal(6, 4, 3)
 
 print(f'หาสี่เหลี่ยม = {reg}')
 print(f'หาสามเหลี่ยม = {tri}')
 print(f'วงกลม = {cir}')
 print(f'ครึ่งวงกลม = {sem}')
+print(f'หาสี่เหลี่ยมจัตุรัส = {squ}')
