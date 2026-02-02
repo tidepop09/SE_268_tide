9bab1badef69201231b6879a851bc69a496ac49b
tidepop09 <tidepop09@gmail.com>
2026-02-02 15:04:19 +0700

    version 1.1 tri_cal()

--- tide.py
+++ tide.py
@@ -1,7 +1,10 @@
 
 def area_cal(x, y):
     reg_cal = x * y
-    return (reg_cal)
+    tri_cal = (1/2) * x * y
+    return (reg_cal, tri_cal)
 
-reg = area_cal(4, 2)
-print(reg)
+
+reg, tri = area_cal(6, 4)
+print(f'หาสี่เหลี่ยม {reg}')
+print(f'หาสามเหลี่ยม {tri}')
